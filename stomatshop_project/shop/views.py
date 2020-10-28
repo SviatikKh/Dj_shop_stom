2d04d83b6403b9e743e055071a979710b8ef3621
SviatikKh <48651894+SviatikKh@users.noreply.github.com>
2020-10-28 21:21:19 +0200

    Added cart template

--- stomatshop_project/shop/views.py
+++ stomatshop_project/shop/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Category, Product
 
 # Create your views here.
+
 def home(request, category_slug=None):
     category_page = None
     products = None
@@ -13,9 +14,14 @@ def home(request, category_slug=None):
         products = Product.objects.all().filter(available=True)
     return render(request, 'home.html', {'category': category_page, 'products': products})
 
+
 def product(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
     except Exception as e:
         raise e
     return render(request, 'product.html', {'product': product})
+
+
+def cart(request):
+    return render(request, 'cart.html')
